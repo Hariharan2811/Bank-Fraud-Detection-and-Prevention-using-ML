62cb143795a7f9a9632f6123f3ca303ac6fcbd69
Hariharan2811 <hariharanvasu28@gmail.com>
2025-12-24 16:23:54 +0530

    Home page Slight Correction

--- Bank/src/Nav.jsx
+++ Bank/src/Nav.jsx
@@ -6,16 +6,18 @@ function Nav() {
             <li>
                 <ul id='List_items_title'>
                     <li><a href="#"><img src="/Logo.png" alt="Bank Logo" className="logo"/></a></li>
-                    <li><a href="#"><h2>Bank Application</h2></a></li>
+                    <li><a href="#"><h2>Sample Bank Application</h2></a></li>
                 </ul>
             </li>
             <li>
-                <ul id='list_items_content'>
+                <ul id="list_items_content">
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="#">About Us</a></li>
                     <li><a href="#">Services</a></li>
+                    <li><a href="#">Transactions</a></li>
+                    <li><a href="#">Security</a></li>
                     <li><a href="#">Contact</a></li>
-                    <li><a href="#"><button>Login/Signin</button></a></li>
+                    <li><a href="/login"><button>Login / Sign Up</button></a></li>
                 </ul>
             </li>
         </ul>    
